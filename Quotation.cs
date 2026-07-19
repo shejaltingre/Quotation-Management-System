d20abe5caf25349ae0005f59502cd71f2b9d0e0d
Shejal Tingre <shejaltingre01@gmail.com>
2026-07-19 16:32:15 +0530

    Create Quotation.cs

---
+++ Quotation.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    public class Quotation
+    {
+        [Key]
+        [Required]
